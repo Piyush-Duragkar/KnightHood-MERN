e22736be9dec41dcb591429d89abd949948143b2
Piyush Duragkar <piyushduragkar621@gmail.com>
2025-05-06 15:09:18 -0400

    Added follow functiionality

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,44 @@
+import User from "../models/user.model.js";
+
+export const getUserProfile = async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    const user = await User.findOne({ username }).select("-password");
+    if (!user) return res.status(400).json({ error: "User does not exist." });
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log("Error in getUserProfile: ", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const followUnfollowUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userToModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
+
+    if (id === req.user._id.toString()) {
+      return res.status(400).json({ error: "You cannot follow yourself." });
+    }
+    if (!userToModify) {
+      return res.status(400).json({ error: "User does not exist." });
+    }
+    const isFollowing = currentUser.following.includes(id);
+
+    if (isFollowing) {
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+      res.status(200).json({ message: "User unfollowed successfully." });
+    } else {
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+      res.status(200).json({ message: "User followed successfully." });
+    }
+  } catch (error) {
+    console.log("Error in follow and unfollow controller: ", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
-
-import connectMongoDB from "../db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectMongoDB from "../db/connectMongoDB.js";
 
 dotenv.config();
 
@@ -18,10 +18,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
-app.listen(PORT, () =>{
-    console.log(`Server running on ${PORT}`);
-    connectMongoDB();
-})
-
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+  connectMongoDB();
+});
