6c5164706e2923fbad0ad42b8dda3dddbc0f16e8
Piyush Duragkar <piyushduragkar621@gmail.com>
2025-05-07 16:11:15 -0400

    Modified Auth Routes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,18 +1,20 @@
-import express from 'express';
-import { getMe, login, logout, signup } from '../controllers/auth.controller.js';
-import { protectRoute } from '../middleware/protectRoute.js';
-
-
+import express from "express";
+import {
+  getMe,
+  login,
+  logout,
+  signup,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 
-router.get("/me",protectRoute, getMe);
+router.get("/me", protectRoute, getMe);
 
 router.post("/login", login);
 
 router.post("/logout", logout);
-
 
 export default router;
