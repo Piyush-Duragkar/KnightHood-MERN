668378d5b48804a85ae668df37a2bc8b18340c43
Piyush Duragkar <piyushduragkar621@gmail.com>
2025-05-09 19:37:00 -0400

    Restructured frontend folderes

--- db/connectMongoDB.js
+++ db/connectMongoDB.js
@@ -1,18 +1,16 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-
 dotenv.config();
 
-
-const connectMongoDB = async() =>{
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error connecting to mongodb: ${error.message}`);
-        process.exit(1);
-    }
+const connectMongoDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to mongodb: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 export default connectMongoDB;
