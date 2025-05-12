4ba9f4c4a704b126e6084f545f16360978ba3ee4
Piyush Duragkar <piyushduragkar621@gmail.com>
2025-05-12 18:52:19 -0400

    partial working tailwind and daisyui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-
+import "./App.css";
 function App() {
   return (
-    <div className="min-h-screen bg-base-100 text-base-content">
-      <button className="btn btn-primary">Button</button>
+    <div className="min-h-screen bg-base-200 text-base-content">
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-myColor">Button</button>
       <button className="btn btn-neutral">Neutral</button>
       <button className="btn btn-primary">Primary</button>
       <button className="btn btn-secondary">Secondary</button>
@@ -12,48 +13,7 @@ function App() {
       <button className="btn btn-success">Success</button>
       <button className="btn btn-warning">Warning</button>
       <button className="btn btn-error">Error</button>
-
-      <input
-        type="checkbox"
-        defaultChecked
-        className="checkbox checkbox-primary"
-      />
-      <input
-        type="checkbox"
-        defaultChecked
-        className="checkbox checkbox-secondary"
-      />
-      <input
-        type="checkbox"
-        defaultChecked
-        className="checkbox checkbox-accent"
-      />
-      <input
-        type="checkbox"
-        defaultChecked
-        className="checkbox checkbox-neutral"
-      />
-
-      <input
-        type="checkbox"
-        defaultChecked
-        className="checkbox checkbox-info"
-      />
-      <input
-        type="checkbox"
-        defaultChecked
-        className="checkbox checkbox-success"
-      />
-      <input
-        type="checkbox"
-        defaultChecked
-        className="checkbox checkbox-warning"
-      />
-      <input
-        type="checkbox"
-        defaultChecked
-        className="checkbox checkbox-error"
-      />
+      <button className="btn btn-mycolor">Black</button>
     </div>
   );
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss({
-      config: "./tailwind.config.mjs", // Point to your config file
-    }),
-  ],
+  plugins: [react()],
   server: {
     port: 3000,
   },
