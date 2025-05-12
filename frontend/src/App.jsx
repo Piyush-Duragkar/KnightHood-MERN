4807e8055f9f06ed8fa808ad14eacd3b93ab0871
Piyush Duragkar <piyushduragkar621@gmail.com>
2025-05-12 19:35:27 -0400

    working tailwind and daisy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,20 @@ import { useState } from "react";
 import "./App.css";
 function App() {
   return (
-    <div className="min-h-screen bg-base-200 text-base-content">
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-myColor">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
-      <button className="btn btn-mycolor">Black</button>
+    <div bg-color="base-100">
+      <div class="bg-base-100 border-base-300 text-base-content">
+        <button className="btn btn-secondary">Button</button>
+        <button className="btn btn-neutral">Button</button>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-mycolor">Info</button>
+        <button className="btn btn-success">Success</button>
+        <button className="btn btn-warning">Warning</button>
+        <button className="btn btn-error">Error</button>
+        <button className="btn btn-mycolor">Black</button>
+      </div>
     </div>
   );
 }
